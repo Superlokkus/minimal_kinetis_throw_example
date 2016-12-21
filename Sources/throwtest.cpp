9bbfbd18dfcab64cd32b1f87bec2d685f7102af3
Superlokkus <m.klemm@novum-engineering.com>
2016-12-21 15:39:30 +0100

    Even more minimalized

--- Sources/throwtest.cpp
+++ Sources/throwtest.cpp
@@ -1,28 +1,9 @@
-/*
- * throwtest.cpp
- * Copyright (c) 2015, Novum engineerING GmbH
- * All rights reserved.
- *
- *  Created on: 21.12.2016
- *      Author: Sören Birth
- */
-
 #include <throwtest.h>
 
-struct destruct_tester {
-	int i = 5;
-	~destruct_tester() {
-		i = 0;
-	}
-};
-
 void thrower() {
-	int i = 3;
 	try {
-		destruct_tester dt;
 		throw 4;
 	} catch (...) {
-		i = 2;
 	}
 }
 
--- Sources/throwtest.h
+++ Sources/throwtest.h
@@ -1,12 +1,3 @@
-/*
- * throwtest.h
- * Copyright (c) 2015, Novum engineerING GmbH
- * All rights reserved.
- *
- *  Created on: 21.12.2016
- *      Author: Sören Birth
- */
-
 #ifndef SOURCES_THROWTEST_H_
 #define SOURCES_THROWTEST_H_
 
